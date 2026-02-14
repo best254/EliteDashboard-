f057db0abbb6cc939f1b486a77120f510dc76a8f
best254 <mikechallo7@gmail.com>
2026-02-14 04:31:21 +0300

    Update app.py for auth functionality

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-import subprocess
 
 app = Flask(__name__)
 
@@ -7,21 +6,5 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/run/superapp")
-def run_superapp():
-    out = subprocess.getoutput("python ~/DemoBrain/app/safe_demo_script.py")
-    return f"<pre>{out}</pre><br><a href='/'>Back</a>"
-
-@app.route("/run/tracker")
-def run_tracker():
-    out = subprocess.getoutput("python ~/DemoBrain/tracker/demo_tracker.py")
-    return f"<pre>{out}</pre><br><a href='/'>Back</a>"
-
-@app.route("/run/system")
-def run_system():
-    out = subprocess.getoutput("python ~/DemoBrain/system/demo_system.py")
-    return f"<pre>{out}</pre><br><a href='/'>Back</a>"
-
-app.run(host="0.0.0.0", port=5000)
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, threaded=True)
+    app.run(host="127.0.0.1", port=5000)
